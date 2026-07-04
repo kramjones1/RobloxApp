c03270edb72be86896a559733cb5b22edd82b8cf
anito1431 <anito1431@github.com>
2026-07-04 18:05:35 +0800

    Add free TURN server for mobile data support

--- App.web.tsx
+++ App.web.tsx
@@ -101,6 +101,9 @@ export default function WebApp() {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayprojectsecret' },
+        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayprojectsecret' },
+        { urls: 'turn:openrelay.metered.ca:80?transport=tcp', username: 'openrelayproject', credential: 'openrelayprojectsecret' },
       ],
     });
     pcRef.current = pc;
